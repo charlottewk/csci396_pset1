87f660fabb042fcc74b1a8b8f98bf58275e60c74
Charlotte Walker-Kahne <charlottewalker@protonmail.com>
2019-09-17 14:16:08 -0700

    updated routing.py

--- routing.py
+++ routing.py
@@ -20,25 +20,42 @@ class RoutingTable:
         Creates a new routing table with a single entry indicating
         that this node can reach itself in zero hops.
         """
-        # You complete this implementation
+        self.name = name
+       	self.hop_table = {self.name: 0}
+       	self.neighbors = {}
 
     def getNodeNames(self):
         """
         Returns an alphabetized list of the known destination nodes.
         """
-        # You complete this implementation
+       	node_name_list = list(self.hop_table.keys())
+       	node_name_list.sort()
+       	return node_name_list
 
     def getHopCount(self, destination):
         """
         Returns the hop count from this node to the destination node.
         """
-        # You complete this implementation
+        if destination in self.hop_table:
+        	return self.hop_table[destination]
+        else:
+        	return None
 
     def getBestLink(self, destination):
         """
         Returns the name of the first node on the path to destination.
         """
-        # You complete this implementation
+        best_table = self
+        best_node = self.name
+        hop_tables = self.neighbors.items()
+        for node_name,hop_table in hop_tables:
+        	candidate = hop_table.getHopCount(destination)
+        	current_best = best_table.getHopCount(destination)
+        	if candidate <= current_best:
+        		best_node = node_name
+        		best_table = hop_table
+        return best_node
+
 
     def update(self, source, table):
         """
@@ -46,5 +63,16 @@ class RoutingTable:
         received from the node whose name is given by source.  The table
         parameter is the current RoutingTable object for the source.
         """
-        # You complete this implementation
+       	hop_counts = table.hop_table.items()
+       	for node_name,node_distance in hop_counts:
+       		if node_name != self.name:
+	       		if node_name in self.hop_table:
+	       			if self.getHopCount(node_name) > node_distance:
+	       				self.hop_table[node_name] = node_distance + 1
+	       		else:
+	       			self.hop_table[node_name] = node_distance + 1
+	       		if node_distance + 1 == 1:
+	       			if node_name not in self.neighbors:
+	       				if node_name == source:
+	       					self.neighbors[node_name] = table
       
